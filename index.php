c72252c10c4d3ca8008f345df7c1c11ac0bcfbb1
Vetka921 <vetka921@gmail.com>
2018-06-15 21:10:31 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -9,20 +9,105 @@
   <head>
 
 <title>Main | T/null</title>
+<style>
+body{
+background: url(bg-pattern.png),linear-gradient(to left,#7b4397,#dc2430);
+}
+h2{
+	color:#fdcc52;
+	    font-family: Catamaran,Helvetica,Arial,sans-serif;
+		font-weight:200;
+		text-shadow: 2px 2px 4px black; /* Параметры тени */
+}
+#leftmenu{
+	margin-left:50px;
+	margin-top:30px;
+	background-color:rgba(255,255,255,0.4);
+	padding:10px;
+	min-width:100px;
+	width:250px;
+	-webkit-border-radius:5px;
+	border:1px solid rgba(200,200,200,0.2);
+	-webkit-box-shadow:2px 2px 4px 0px rgba(0,0,0,0.6);
+}
+#menulist a{
+	color:white;
+	font-family: Lato,Helvetica,Arial,sans-serif;
+		font-weight:200;
+		text-decoration:none;
+		list-style-type: none;
+	
+}
+#header{
+	background-image: url(back.jpg) ;
+	background-size: cover;
+	height:160px;
+	width:270px;
+	margin-left:50px;
+	margin-top:30px;
+	-webkit-border-radius:5px;
+	border:1px solid rgba(255,255,255,0.8);
+	-webkit-box-shadow:2px 2px 4px 0px rgba(0,0,0,0.6);
+}
+#header h1{
+	text-align:center;
+	font-family: Consolas,Helvetica,Arial,sans-serif;
+	color:#fdcc52;
+	text-shadow: 2px 2px 4px black; /* Параметры тени */
+}
+#header h4{
+	font-family: Consolas,Helvetica,Arial,sans-serif;
+	font-weight:200;
+	text-align:right;
+	margin-right:5px;
+	padding-right:5px;
+	margin-bottom:3px;
+	color:white;
+	text-shadow: 1px 1px 2px black; /* Параметры тени */
+}
+</style>
+<script>
+window.onload = function(){
+
+    (function(){
+
+
+        var date = new Date();
+
+        var time = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
+		//var date1 = date.getDay()+'.'+date.getMonth()+'.'+date.getFullYear();
+		//var date1 = date.getDate('DD:MM:YYYY');
+		document.getElementById('time_now').innerHTML = time;
+		//document.getElementById('date_now').innerHTML = date1;
+        window.setTimeout(arguments.callee, 1000);
+
+    })();
+
+};
+
+</script>
 
 
   </head>
 
   <body id="page-top">
+  <div id="header">
+  <h1>T/Null lab.</h1>
+  <h4>tools/sources<br>Временное веб убежище<br>Cur. Time: <span id="time_now"></span></h4>
+  </div>
   
+  
+  
+  <div id="leftmenu">
   <h2>Tools</h2>
-  <ul>
-   <a href="translator/"><li>Перевод в Grid Cipher</li></a>
-   <a href="fakegoogle/"><li>Fake Google</li></a>
+  <ul id="menulist">
+   <a href="translator/"><li>> Grid Cipher</li></a>
+   <a href="fakegoogle/"><li>> Fake Google</li></a>
+   <a href="spec_proj/"><li>> New project</li></a>
 
   
   </ul>
-  
+  </div>
   
 
 
--- translator/decode.php
+++ translator/decode.php
@@ -2,16 +2,124 @@
 <html lang="ru">
   <head>
     <title>Secrets.to</title>
+	<style>
+	.btn{
+		height:40px;
+		width:40px;
+		margin:3px;
+		
+	}
+	#btncon{
+		width:330px;
+		align:center;
+	}
+	
+	</style>
   </head>
-<h3>It works only with English</h3>
+<h3>It works only with English - try to <a href="index.php">encode</a>.</h3>
   <body id="page-top">
-    <form action="index.php" method="get">
-  <input type="text" name="text" placeholder="Phrase"/>
-  <input type="submit" value="Encode"/> 
-  
+    <form action="index.php" method="get"><center>
+<div id="btncon">
+  <?php 
+  $alpha = array(
+    1 => "_| ",
+    2 => "|_| ",
+	3 => "|_ ",
+	4 => "] ",
+	5 => "[] ",
+	6 => "[ ",
+	7 => "&macr;| ",
+	8 => "|&macr;| ",
+	9 => "|&macr; ",
+	
+	10 => "&bull;_| ",
+	11 => "|&bull;_| ",
+	12 => "|_&bull; ",
+	13 => "&bull;] ",
+	14 => "[&bull;] ",
+	15 => "[&bull; ",
+	16 => "&bull;&macr;| ",
+	17 => "|&macr;|&bull; ",
+	18 => "|&macr;&bull; ",
+	
+	19 => "> ",
+	20 => "V ",
+	21 => "< ",
+	22 => "&Lambda; ",
+	
+	23 => "&bull;> ",
+	24 => "V&bull; ",
+	25 => "<&bull; ",
+	26 => "&Lambda;&bull; ",
+	27 => "  "
+	
+	
+);
+
+ $alpha1 = array(
+    1 => "a",
+    2 => "b",
+	3 => "c",
+	4 => "d",
+	5 => "e",
+	6 => "f",
+	7 => "g",
+	8 => "h",
+	9 => "i",
+	
+	10 => "j",
+	11 => "k",
+	12 => "l",
+	13 => "m",
+	14 => "n",
+	15 => "o",
+	16 => "p",
+	17 => "q",
+	18 => "r",
+	
+	19 => "s",
+	20 => "t",
+	21 => "u",
+	22 => "v",
+	
+	23 => "w",
+	24 => "x",
+	25 => "y",
+	26 => "z",
+	27 => " "
+	
+	
+);
+for($i=1;$i<28;$i++){
+	
+	
+	
+
+  printf ('<input type="button" id="%s" value="%s" class="btn" onClick="result.value+=\'%s\' "/>',$i,$alpha[$i],$alpha1[$i]);
+  if($i==3 || $i==6 || $i==9 || $i==12 || $i==15 || $i==18 || $i==22 || $i==26){
+	  echo '<br>';
+  }
+  if($i==9 || $i==18 || $i==22 || $i==26 ){
+	  echo '<br>';
+  }
+}  
+  ?><input type="button" id="30" value="CLR" class="btn" onClick="result.value='' "/>
+  </div><br><br>
+  <input type="text" readonly id="result"/>
+  </center>
   </form>
+  	<script>
+	var result = document.getElementById('result');
+	</script>
+  <script>
+  
+  
+  
+  
+  </script>
   <br><br><br>
 <div><?php
+/*
 if(isset($_GET['text'])){$text=$_GET['text'];}
 $text=strtolower($text);
 $artxt=str_split($text);
@@ -59,7 +167,7 @@ $alpha = array(
 for($ini=0;$ini<strlen($text);$ini++){
 	echo $alpha[$artxt[$ini]];
 }
-
+*/
 
 
 
